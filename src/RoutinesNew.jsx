8dc8701ba80c27b2f02f8e48b22597a1118122be
alexisxaltamirano <alexisaltamirano1998@gmail.com>
2024-04-18 13:25:22 -0700

    updated RoutinesNew

--- src/RoutinesNew.jsx
+++ src/RoutinesNew.jsx
@@ -1,8 +1,14 @@
-export function RoutinesNew() {
+/* eslint-disable react/prop-types */
+export function RoutinesNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onCreateRoutine(params, () => event.target.reset());
+  };
   return (
     <div>
       <h2>New Routine</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           Routine Title: <input name="name" type="text" />
         </div>
